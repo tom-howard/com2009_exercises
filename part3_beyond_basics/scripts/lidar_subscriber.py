550aeb74f7fc50cee18b25d20ee1a3617900b0e7
Tom Howard <t.howard@sheffield.ac.uk>
2025-01-29 16:05:51 +0000

    lidar_sub (advanced)

--- part3_beyond_basics/scripts/lidar_subscriber.py
+++ part3_beyond_basics/scripts/lidar_subscriber.py
@@ -23,18 +23,26 @@ class LidarSubscriber(Node):
         self.get_logger().info(f"The '{self.get_name()}' node is initialised.")
 
     def lidar_callback(self, scan_data: LaserScan): 
-        left = scan_data.ranges[0:11]
-        right = scan_data.ranges[-10:]
-        front = np.array(left[::-1] + right[::-1])
-        
-        valid_data = front[front != float("inf")]
-        if np.shape(valid_data)[0] > 0:
-            single_point_average = valid_data.mean()
-        else:
-            single_point_average = float("nan")
+        left_20_deg = scan_data.ranges[0:21]
+        right_20_deg = scan_data.ranges[-20:]
+        front = np.array(left_20_deg + right_20_deg)
+
+        front = front[front != float("inf")]
+        front_spa = front.mean() if np.shape(front)[0] > 0 else float("nan")
+
+        left = np.array(scan_data.ranges[80:101])
+        left = left[left != float("inf")]
+        left_spa = left.mean() if np.shape(left)[0] > 0 else float("nan")
+
+        right = np.array(scan_data.ranges[260:281])
+        right = right[right != float("inf")]
+        right_spa = right.mean() if np.shape(right)[0] > 0 else float("nan")
 
         self.get_logger().info(
-            f"LiDAR Reading (front): {single_point_average:.3f} meters.",
+            f"LiDAR Readings:\n"
+            f"  Front: {front_spa:.3f} meters\n"
+            f"  Left: {left_spa:.3f} meters\n"
+            f"  Right: {right_spa:.3f} meters\n",
             throttle_duration_sec = 1,
         )
     
