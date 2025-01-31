2b7450e005a4c536a9067734b7c432d9cf6937b2
Tom Howard <t.howard@sheffield.ac.uk>
2025-01-31 16:24:42 +0000

    new print format

--- part1_pubsub/scripts/subscriber.py
+++ part1_pubsub/scripts/subscriber.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # A simple ROS2 Subscriber
 
-import rclpy
+import rclpy 
 from rclpy.node import Node
 
 from example_interfaces.msg import String
@@ -9,22 +9,27 @@ from example_interfaces.msg import String
 class SimpleSubscriber(Node): 
 
     def __init__(self): 
-        super().__init__("simple_subscriber")
+        super().__init__("simple_subscriber") 
 
         self.my_subscriber = self.create_subscription(
             msg_type=String,
             topic="my_topic",
             callback=self.msg_callback,
             qos_profile=10,
-        )
-        
-        self.get_logger().info(f"The '{self.get_name()}' node is initialised.")
+        ) 
 
-    def msg_callback(self, topic_message): 
-        self.get_logger().info(f"The '{self.get_name()}' node heard:")
-        self.get_logger().info(f"'{topic_message.data}'")
-    
-def main(args=None):
+        self.get_logger().info(
+            f"The '{self.get_name()}' node is initialised."
+        ) 
+
+    def msg_callback(self, topic_message: String): 
+        
+        self.get_logger().info(
+            f"\nThe '{self.get_name()}' node heard:\n"
+            f"  '{topic_message.data}'"
+        )
+
+def main(args=None): 
     rclpy.init(args=args)
     my_simple_subscriber = SimpleSubscriber()
     rclpy.spin(my_simple_subscriber)
