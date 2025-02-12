3179748bc8b0478d30a72f653c296c3edf6398fe
Tom Howard <t.howard@sheffield.ac.uk>
2025-02-12 13:41:21 +0000

    tweaks for final version

--- part4_services/scripts/number_game_client.py
+++ part4_services/scripts/number_game_client.py
@@ -11,44 +11,55 @@ class NumberGameClient(Node):
 
     def __init__(self):
         super().__init__('number_game_client')
+        
         self.client = self.create_client(
             srv_type=MyNumberGame, 
             srv_name='guess_the_number'
         )
-        cli = argparse.ArgumentParser(...)
+        
+        cli = argparse.ArgumentParser()
         cli.add_argument(
-            "--guess",
-            default=0,
-            type=int,
+            "-g", "--guess", default=0, type=int
         )
         cli.add_argument(
-            "--cheat",
-            default=False,
-            type=bool,
+            "-c", "--cheat", action="store_true"
         )
         self.args = cli.parse_args()
+        
         while not self.client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info(
-                'service not available, waiting again...'
+                "Waiting for service..."
             )
-        self.request = MyNumberGame.Request()
 
     def send_request(self, guess, cheat):
-        self.request.guess = guess
-        self.request.cheat = cheat
-        return self.client.call_async(self.request)
+        request = MyNumberGame.Request()
+        request.guess = guess
+        request.cheat = cheat
+        
+        return self.client.call_async(request)
 
 def main():
     rclpy.init()
-
     client = NumberGameClient()
+
+    client.get_logger().info(
+        f"Sending the request:\n"
+        f" - guess: {client.args.guess}\n"
+        f" - cheat: {client.args.cheat}\n"
+        f"   Awaiting response..."
+    )
+    
     future = client.send_request(client.args.guess, client.args.cheat)
     rclpy.spin_until_future_complete(client, future)
     response = future.result()
+    
     client.get_logger().info(
-        f"Result:\n"
-        f" - {response.hint}"
+        f"The server has responded with:\n"
+        f" - {'Correct :)' if response.correct else 'Incorrect :('}\n"
+        f" - Number of attempts so far: {response.num_guesses}\n"
+        f" - A hint for your next guess: '{response.hint}'."
     )
+    
     client.destroy_node()
     rclpy.shutdown()
 
